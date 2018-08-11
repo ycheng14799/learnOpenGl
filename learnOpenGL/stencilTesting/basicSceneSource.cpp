c32e974127e58e6d54bbee836dba9393900c0cc2
Yi Fei Cheng <ycheng2@swarthmore.edu>
2018-08-10 23:54:48 -0400

    fixed bug. rendered basic scene for stencil and depth testing guides.

--- learnOpenGL/stencilTesting/basicSceneSource.cpp
+++ learnOpenGL/stencilTesting/basicSceneSource.cpp
@@ -19,13 +19,13 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 // camera
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
+float lastX = (float)SCR_WIDTH / 2.0f;
+float lastY = (float)SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
 
 // timing
@@ -46,7 +46,7 @@ int main() {
 
 	GLFWwindow * window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
-	{ 
+	{
 		cout << "Failed to create GLFW window" << endl;
 		glfwTerminate();
 		return -1;
@@ -55,81 +55,81 @@ int main() {
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_callback);
 	glfwSetScrollCallback(window, scroll_callback);
-	
-	// GLFW set to capture mouse 
+
+	// GLFW set to capture mouse
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	
-	// glad: load all OpenGL function pointers 
+
+	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		cout << "Failed to initialize GLAD" << endl;
 		return -1;
 	}
-	
-	// Configure global openGL state 
+
+	// Configure global openGL state
 	glEnable(GL_DEPTH_TEST);
-	
-	// Build and compile shader program 
+
+	// Build and compile shader program
 	Shader shader("basicScene.vs", "basicScene.fs");
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
-    	// ------------------------------------------------------------------
-    	float cubeVertices[] = {
-        	// positions          // texture Coords
-        	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-         	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-         	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-        	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    // ------------------------------------------------------------------
+    float cubeVertices[] = {
+        // positions          // texture Coords
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-        	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-         	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-        	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-        	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-        	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-       	 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-        	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-         	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-         	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-        	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-         	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-         	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-         	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-        	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-        	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-        	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-         	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-         	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-         	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-        	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-    	};
-    	float planeVertices[] = {
-       		// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
-         	5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        	-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-        	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    float planeVertices[] = {
+        // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
 
-        	5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-        	-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
-         	5.0f, -0.5f, -5.0f,  2.0f, 2.0f								
-    	};	
-	
-	// Cube VAO 
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+    };
+
+	// Cube VAO
 	unsigned int cubeVAO, cubeVBO;
 	glGenVertexArrays(1, &cubeVAO);
 	glGenBuffers(1, &cubeVBO);
@@ -141,11 +141,11 @@ int main() {
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glBindVertexArray(0);
-	
-	// Plane VAO 
+
+	// Plane VAO
 	unsigned int planeVAO, planeVBO;
 	glGenVertexArrays(1, &planeVAO);
-	glGenBuffers(1, &planeVBO);	
+	glGenBuffers(1, &planeVBO);
 	glBindVertexArray(planeVAO);
 	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
@@ -153,122 +153,125 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0); 
-	
-	// Load Textures 
-	// Load cubeTexture 
+	glBindVertexArray(0);
+
+	// Load Textures
+	// Load cubeTexture
 	unsigned int cubeTexture;
-	glGenTextures(1, &cubeTexture);	 
-	// Bind texture 
+	glGenTextures(1, &cubeTexture);
+	// Bind texture
 	glBindTexture(GL_TEXTURE_2D, cubeTexture);
-	// Set texture wrapping / filtering options 
+	// Set texture wrapping / filtering options
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	// Set texture filtering parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// Load data from image and generate texture 
+	// Load data from image and generate texture
 	int width, height, nrChannels;
 	unsigned char *data = stbi_load("marble.jpg", &width, &height, &nrChannels, 0);
-	if (data) { 
+	if (data) {
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
+
+		// Free data
+		stbi_image_free(data);
 	}
-	else 
+	else
 	{
 		cout << "Failed to load texture" << endl;
 		return -1;
-	}	
-	// Free data 
-	stbi_image_free(data);
-	
+	}
+
 	// Plane texture
 	unsigned int planeTexture;
         glGenTextures(1, &planeTexture);
-        // Bind texture 
+        // Bind texture
         glBindTexture(GL_TEXTURE_2D, planeTexture);
-        // Set texture wrapping / filtering options 
+        // Set texture wrapping / filtering options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         // Set texture filtering parameters
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        // Load data from image and generate texture 
+        // Load data from image and generate texture
         data = stbi_load("metal.png", &width, &height, &nrChannels, 0);
         if (data) {
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
                 glGenerateMipmap(GL_TEXTURE_2D);
+
+        	// Free data
+        	stbi_image_free(data);
         }
         else
         {
                 cout << "Failed to load texture" << endl;
 		return -1;
         }
-        // Free data 
-        stbi_image_free(data);
-	
+
 	// shader configuration
 	shader.use();
 	shader.setInt("texture1", 0);
-	
+
 	// render loop
 	while(!glfwWindowShouldClose(window))
 	{
-		// per-frame time logic 
+		// per-frame time logic
 		float currentFrame = glfwGetTime();
 		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;	
-		
+		lastFrame = currentFrame;
+
 		// input
 		processInput(window);
-		
+
 		// render
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		
-		shader.use();
-		glm::mat4 model;
-		glm::mat4 view = camera.GetViewMatrix();
-		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		shader.setMat4("view", view);
-		shader.setMat4("projection", view);
-		
-		// cubes 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    shader.use();
+    glm::mat4 model;
+    glm::mat4 view = camera.GetViewMatrix();
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    shader.setMat4("view", view);
+    shader.setMat4("projection", projection);
+
+		// cubes
 		glBindVertexArray(cubeVAO);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture);
-		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
-		model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-		shader.setMat4("model", model);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture);
+    model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+    shader.setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    model = glm::mat4();
+    model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+    shader.setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 		// floor
 		glBindVertexArray(planeVAO);
-		glBindTexture(GL_TEXTURE_2D, planeTexture);
-		shader.setMat4("model", glm::mat4());
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindVertexArray(0);
-		
+    glBindTexture(GL_TEXTURE_2D, planeTexture);
+    shader.setMat4("model", glm::mat4());
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+  	glBindVertexArray(0);
+
+
 		// glfw: Swap buffers and poll IO events
 		glfwSwapBuffers(window);
-		glfwPollEvents();	
+		glfwPollEvents();
 	}
-	
-	// De-allocate resources 
+
+	// De-allocate resources
 	glDeleteVertexArrays(1, &cubeVAO);
 	glDeleteVertexArrays(1, &planeVAO);
 	glDeleteBuffers(1, &cubeVBO);
 	glDeleteBuffers(1, &planeVBO);
-	
+
 	glfwTerminate();
-	return 0; 
+	return 0;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
+    // make sure the viewport matches the new window dimensions; note that width and
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
@@ -310,5 +313,3 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
-
-
